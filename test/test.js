bebc6b817ba3f2c1f3886e524a98fc7f78774e5e
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2014-03-28 16:40:10 -0700

    add test and travis

---
+++ test/test.js
@@ -0,0 +1,24 @@
+var page = require('webpage').create();
+var _ = require('underscore');
+
+var sawExpectedConsoleLogs = {
+	"got customLibrary2: undefined" : false,
+	"we got underscore!" : false
+};
+
+page.open('http://localhost:3000/page1', function() {
+});
+
+page.onConsoleMessage = function(msg, lineNum, sourceId) {
+	sawExpectedConsoleLogs[ msg ] = true;
+};
+
+setTimeout( function() {
+	if( _.values( sawExpectedConsoleLogs ).indexOf( false ) !== -1 ) {
+		console.log( "not everything we expected was console logged" );
+		phantom.exit( 1 );
+	}
+	else {
+		phantom.exit( 0 );
+	}
+}, 1000 );
